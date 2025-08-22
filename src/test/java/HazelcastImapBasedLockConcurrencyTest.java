ff838eb97b7037d291979a4cb1bcb64d7fb68c52
Fabrizio Cannizzo <fabrizio.cannizzo@hazelcast.com>
2025-08-22 11:54:18 +0100

    implemented lock using ISemaphore

--- src/test/java/HazelcastImapBasedLockConcurrencyTest.java
+++ src/test/java/HazelcastImapBasedLockConcurrencyTest.java
@@ -1,21 +1,28 @@
 import com.hazelcast.config.Config;
+import com.hazelcast.config.cp.CPSubsystemConfig;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.map.IMap;
+import com.voghbum.CpSemaphoreUnownedLock;
 import com.voghbum.HazelcastImapBasedUnownedLock;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.function.Executable;
 
+import java.time.Duration;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.locks.Lock;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class HazelcastImapBasedLockConcurrencyTest {
-    private static HazelcastInstance hazelcastInstance;
-    private static final String LICENSE = "ENTER LICENSE KEY";
+    private static HazelcastInstance hz1, hz2, hz3;
+    private static final String LICENSE = System.getenv("HZ_LICENSEKEY");
     private static final String MAP_KEY = "test-map";
     private static final String LOCK_NAME = "test-lock";
 
@@ -24,62 +31,55 @@ public class HazelcastImapBasedLockConcurrencyTest {
         Config config = new Config();
         config.setClusterName("test-cluster");
         config.setLicenseKey(LICENSE);
-        hazelcastInstance = Hazelcast.newHazelcastInstance(config);
+        CPSubsystemConfig cp = new CPSubsystemConfig()
+                .setCPMemberCount(3)
+                .setGroupSize(3)
+                .setSessionHeartbeatIntervalSeconds(1)
+                .setSessionTimeToLiveSeconds(15);
+        config.setCPSubsystemConfig(cp);
+
+        hz1 = Hazelcast.newHazelcastInstance(config);
+        hz2 = Hazelcast.newHazelcastInstance(config);
+        hz3 = Hazelcast.newHazelcastInstance(config);
     }
 
     @AfterAll
     static void teardown() {
-        hazelcastInstance.shutdown();
+        hz1.shutdown();
+        hz2.shutdown();
+        hz3.shutdown();
     }
 
     @Test
-    void shouldUnlockAnotherThreadCorrectlyWhenLowLoad() throws InterruptedException {
-        IMap<String, String> map = hazelcastInstance.getMap(MAP_KEY);
+    void shouldUnlockAnotherThreadCorrectlyWhenLowLoad_map() throws InterruptedException {
+        IMap<String, String> map = hz1.getMap(MAP_KEY);
         HazelcastImapBasedUnownedLock lock = new HazelcastImapBasedUnownedLock(map, LOCK_NAME);
-        int threadCount = 2;
-        int taskCount = 9;
-        ExecutorService executor = Executors.newFixedThreadPool(threadCount);
-        ExecutorService unlockerExecutor = Executors.newFixedThreadPool(threadCount);
-        CountDownLatch doneLatch = new CountDownLatch(threadCount);
-        AtomicLong count = new AtomicLong(0);
-        AtomicInteger failCount = new AtomicInteger(0);
+        shouldUnlockAnotherThreadCorrectly(2, 9, lock);
+    }
 
-        for (int i = 0; i < taskCount; i++) {
-            executor.submit(() -> {
-                try {
-                    lock.lock();
-                    var current = count.incrementAndGet();
-                    if(current != 1) {
-                        failCount.incrementAndGet();
-                    }
-                    // Another thread release the lock
-                    unlockerExecutor.submit(() -> {
-                        try {
-                            count.decrementAndGet();
-                            lock.unlock();
-                        } finally {
-                            doneLatch.countDown();
-                        }
-                    });
-                } catch (Exception e) {
-                    doneLatch.countDown();
-                }
-            });
-        }
-
-        doneLatch.await();
-        executor.shutdown();
-
-        assertEquals(0, failCount.get(), "Critical section violation!");
+    @Test
+    void shouldUnlockAnotherThreadCorrectlyWhenLowLoad_sem() throws InterruptedException {
+        CpSemaphoreUnownedLock lock = new CpSemaphoreUnownedLock(hz1, LOCK_NAME);
+        shouldUnlockAnotherThreadCorrectly(2, 9, lock);
     }
 
     // IT FAILS
     @Test
-    void shouldUnlockAnotherThreadCorrectly() throws InterruptedException {
-        IMap<String, String> map = hazelcastInstance.getMap(MAP_KEY);
+    void shouldUnlockAnotherThreadCorrectly_map() throws InterruptedException {
+        IMap<String, String> map = hz1.getMap(MAP_KEY);
         HazelcastImapBasedUnownedLock lock = new HazelcastImapBasedUnownedLock(map, LOCK_NAME);
-        int threadCount = 100;
-        int taskCount = 500;
+        shouldUnlockAnotherThreadCorrectly(100, 500, lock);
+    }
+
+    // IT WORKS
+    @Test
+    void shouldUnlockAnotherThreadCorrectly_sem() throws InterruptedException {
+        CpSemaphoreUnownedLock lock = new CpSemaphoreUnownedLock(hz1, LOCK_NAME);
+        shouldUnlockAnotherThreadCorrectly(200, 2500, lock);
+    }
+
+    private static void shouldUnlockAnotherThreadCorrectly(int threadCount, int taskCount, Lock lock)
+            throws InterruptedException {
         ExecutorService executor = Executors.newFixedThreadPool(threadCount);
         ExecutorService unlockerExecutor = Executors.newFixedThreadPool(threadCount);
         CountDownLatch doneLatch = new CountDownLatch(threadCount);
@@ -91,7 +91,7 @@ public class HazelcastImapBasedLockConcurrencyTest {
                 try {
                     lock.lock();
                     var current = count.incrementAndGet();
-                    if(current != 1) {
+                    if (current != 1) {
                         failCount.incrementAndGet();
                     }
                     // Another thread release the lock
@@ -117,7 +117,7 @@ public class HazelcastImapBasedLockConcurrencyTest {
 
     @Test
     void shouldReleaseTheLockSameThread() throws InterruptedException {
-        IMap<String, String> map = hazelcastInstance.getMap(MAP_KEY);
+        IMap<String, String> map = hz1.getMap(MAP_KEY);
         HazelcastImapBasedUnownedLock lock = new HazelcastImapBasedUnownedLock(map, LOCK_NAME);
 
         int threadCount = 100;
